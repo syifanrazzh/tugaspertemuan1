4edd79c9b89233ab8809cd5cb3e73bc837da6798
Syifa Nur Azizah Suhud-2305056 <syifa.nas31@gmail.com>
2025-02-12 09:12:25 +0700

    Add files via upload

---
+++ BelajarJava.java
@@ -0,0 +1,6 @@
+public class BelajarJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hallo Word Syifa Nur Azizah Suhud 2305056. Saya senang belajar Java");
+    }
+}
